dbb64667a72627eb18e2bd6799738742f481da2e
Simon Blanke <simon.blanke@yahoo.com>
2021-03-28 16:26:27 +0200

    rework ltm

--- hyperactive_long_term_memory/ltm.py
+++ hyperactive_long_term_memory/ltm.py
@@ -16,51 +16,8 @@ def atomic_overwrite(filename):
 
 
 class DataIO:
-    def __init__(self, path=None, save_on="finish"):
+    def __init__(self, path=None):
         pass
-
-    def clean_files(self):
-        for ltm_dir in self.ltm_dirs:
-            search_data_proc_paths = glob.glob(ltm_dir + "search_data_*.csv")
-            search_data_paths = glob.glob(ltm_dir + "search_data*.csv")
-
-            search_data_list = []
-            for search_data_path in search_data_paths:
-                search_data_ = pd.read_csv(search_data_path)
-                search_data_list.append(search_data_)
-
-            if len(search_data_list) > 0:
-                search_data = pd.concat(search_data_list, axis=0)
-                search_data.to_csv(ltm_dir + "search_data.csv", index=False)
-
-            # remove search_data_* files
-            for search_data_proc_path in search_data_proc_paths:
-                if os.path.exists(search_data_proc_path):
-                    os.remove(search_data_proc_path)
-
-    def _get_data_types(self, search_space):
-        self.search_space = search_space
-        self.para_names = list(search_space.keys())
-
-        self.data_types = {}
-        for para_name in search_space.keys():
-            value0 = search_space[para_name][0]
-            print("\npara_name", para_name)
-
-            print("value0", value0, type(value0))
-            if isinstance(value0, numbers.Number):
-                type0 = "number"
-            elif isinstance(value0, str):
-                type0 = "string"
-            elif callable(value0):
-                type0 = "function"
-            else:
-                type0 = None
-                print("Warning! data type of ", para_name, " not recognized")
-
-            self.data_types[para_name] = type0
-
-        print("\n data_types \n", self.data_types, "\n")
 
     def _init_data_path(self, objective_function, nth_process):
         # create file name for processes
@@ -91,7 +48,7 @@ class DataIO:
     def _load(self):
         if os.path.isfile(self.ltm_user_path + self.file_name):
             search_data = pd.read_csv(self.ltm_user_path + self.file_name)
-            """
+
             # conv back to func
             for para_name in self.data_types.keys():
                 data_type = self.data_types[para_name]
@@ -103,13 +60,17 @@ class DataIO:
                 for func in self.search_space[para_name]:
                     func_name = func.__name__
 
-                    with open(self.ltm_user_path + func_name + ".pkl", "rb") as file:
-                        func = pickle.load(file)
+                    # if not os.path.isfile(self.ltm_user_path + func_name + ".pkl"):
+                    #     continue
+
+                    # with open(self.ltm_user_path + func_name + ".pkl", "rb") as file:
+                    #     func = pickle.load(file)
 
                     func_replace[func_name] = func
 
+            print("\n func_replace \n", func_replace)
+
             search_data[para_name] = search_data[para_name].replace(func_replace)
-            """
 
             return search_data
 
@@ -147,6 +108,8 @@ class DataIO:
         for ltm_dir in self.ltm_dirs:
             search_data = pd.read_csv(ltm_dir + self.file_name)
             search_data_new = pd.DataFrame(para_dict, index=[0])
+            search_data_new = self._conv_func(search_data_new, ltm_dir)
+
             search_data = search_data.append(search_data_new)
 
             with atomic_overwrite(ltm_dir + self.file_name) as f:
@@ -159,11 +122,9 @@ class LongTermMemory(DataIO):
         model_id,
         experiment_id="default",
         path=None,
-        save_on="finish",
         backup=False,
     ):
-        super().__init__(path, save_on)
-        self.save_on = save_on
+        super().__init__(path)
 
         self.paths = [path]
 
@@ -183,3 +144,63 @@ class LongTermMemory(DataIO):
 
         for path in self.paths:
             self.ltm_dirs.append(path + self.model_path)
+
+    def clean_files(self):
+        for ltm_dir in self.ltm_dirs:
+            search_data_proc_paths = glob.glob(ltm_dir + "search_data_*.csv")
+            search_data_paths = glob.glob(ltm_dir + "search_data*.csv")
+
+            search_data_list = []
+            for search_data_path in search_data_paths:
+                search_data_ = pd.read_csv(search_data_path)
+                search_data_list.append(search_data_)
+
+            if len(search_data_list) > 0:
+                search_data = pd.concat(search_data_list, axis=0)
+                search_data.to_csv(ltm_dir + "search_data.csv", index=False)
+
+            # remove search_data_* files
+            for search_data_proc_path in search_data_proc_paths:
+                if os.path.exists(search_data_proc_path):
+                    os.remove(search_data_proc_path)
+
+    def init_data_types(self, search_space):
+        self.search_space = search_space
+        self.para_names = list(search_space.keys())
+
+        self.search_space_ltm = {}
+        self.data_types = {}
+        for para_name in search_space.keys():
+            value0 = search_space[para_name][0]
+
+            if isinstance(value0, numbers.Number):
+                type0 = "number"
+                search_dim_ltm = search_space[para_name]
+
+            elif isinstance(value0, str):
+                type0 = "string"
+                search_dim_ltm = search_space[para_name]
+
+            elif callable(value0):
+                type0 = "function"
+
+                search_dim_ltm = []
+                for func in list(search_space[para_name]):
+                    search_dim_ltm.append(func.__name__)
+            else:
+                type0 = None
+                search_dim_ltm = search_space[para_name]
+                print("Warning! data type of ", para_name, " not recognized")
+                print("Memory will not work")
+
+            self.data_types[para_name] = type0
+            self.search_space_ltm[para_name] = search_dim_ltm
+
+    def load(self):
+        return self._load()
+
+    def save_on_finish(self, dataframe):
+        self._save(dataframe)
+
+    def save_on_iteration(self, data_dict):
+        self._append(data_dict)
