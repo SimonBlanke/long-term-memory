668f99b3f263d895cdb4646a2b461b909c242b25
Simon Blanke <simon.blanke@yahoo.com>
2021-05-25 11:39:53 +0200

    rename multiple variables + refactor code into Paths-class

--- hyperactive_long_term_memory/data_io.py
+++ hyperactive_long_term_memory/data_io.py
@@ -5,31 +5,85 @@
 
 import os
 import dill
+import contextlib
 import pandas as pd
 
 
-class DataIO:
+@contextlib.contextmanager
+def atomic_overwrite(filename):
+    temp = filename + "~"
+    with open(temp, "w") as f:
+        yield f
+    os.rename(temp, filename)  # this will only happen if no exception was raised
+
+
+class Paths:
     def __init__(self, path):
         self.path = path
         self.ltm_path = self.path + "/ltm_data/"
 
-    def expermiments(self):
+    def init_paths(self, study_id, model_id):
+        self.ltm_path = self.path + "/ltm_data/"
+        self.model_path_ = "/" + study_id + "/" + model_id + "/"
+        self.file_name_ = "search_data.csv"
+
+        self.model_path = self.ltm_path + self.model_path_
+        self.search_data_path = self.ltm_path + self.model_path_ + self.file_name_
+
+        # create directories if they do not exist
+        if not os.path.exists(self.model_path):
+            os.makedirs(self.model_path, exist_ok=True)
+        elif not os.path.exists(self.ltm_path):
+            os.makedirs(self.ltm_path, exist_ok=True)
+
+
+class DataIO(Paths):
+    def __init__(self, path):
+        super().__init__(path)
+
+    def study_list(self):
         return os.listdir(self.ltm_path)
 
-    def objective_functions(self, study_id):
-        return os.listdir(self.ltm_path + study_id + "/")
+    def model_list(self, exp_id):
+        return os.listdir(self.ltm_path + exp_id + "/")
 
-    def search_data(self, study_id, model_id):
+    def read_search_data(self, study_id, model_id):
         search_data_path = (
             self.ltm_path + study_id + "/" + model_id + "/search_data.csv"
         )
         return pd.read_csv(search_data_path)
 
-    def objective_function(self, experiment_id, model_id):
+    def save_search_data(self, search_data, drop_duplicates):
+        search_data = self.sd_conv.func2str(search_data)
+
+        search_data_loaded = self._load()
+        if not self.replace_existing and search_data_loaded is not None:
+            search_data = pd.concat([search_data, search_data_loaded], axis=0)
+
+        if drop_duplicates:
+            search_data.drop_duplicates(subset=self.para_names, inplace=True)
+
+        with atomic_overwrite(self.search_data_path) as f:
+            search_data.to_csv(f, index=False)
+
+    def read_objective_function(self, study_id, model_id):
         objective_function_path = (
-            self.ltm_path + experiment_id + "/" + model_id + "/objective_function.pkl"
+            self.ltm_path + study_id + "/" + model_id + "/objective_function.pkl"
         )
         with open(objective_function_path, "rb") as input_file:
             objective_function = dill.load(input_file)
 
         return objective_function
+
+    def save_objective_function(self, study_id, model_id):
+        with open(
+            self.ltm_path
+            + "/"
+            + study_id
+            + "/"
+            + model_id
+            + "/"
+            + "objective_function.pkl",
+            "wb",
+        ) as output_file:
+            dill.dump(self.objective_function, output_file)
--- hyperactive_long_term_memory/ltm.py
+++ hyperactive_long_term_memory/ltm.py
@@ -3,9 +3,7 @@
 # License: MIT License
 
 import os
-import dill
 import glob
-import contextlib
 import pandas as pd
 
 from .search_data_converter import SearchDataConverter
@@ -15,38 +13,9 @@ from .data_io import DataIO
 pd.options.mode.chained_assignment = None
 
 
-@contextlib.contextmanager
-def atomic_overwrite(filename):
-    temp = filename + "~"
-    with open(temp, "w") as f:
-        yield f
-    os.rename(temp, filename)  # this will only happen if no exception was raised
-
-
-class LongTermMemory(DataIO):
+class LTMBackend(DataIO):
     def __init__(self, path):
         super().__init__(path)
-
-    def _init_paths(self, model_id, study_id):
-        self.ltm_path = self.path + "/ltm_data/"
-        self.model_path_ = "/" + study_id + "/" + model_id + "/"
-        self.file_name_ = "search_data.csv"
-
-        self.model_path = self.ltm_path + self.model_path_
-        self.search_data_path = self.ltm_path + self.model_path_ + self.file_name_
-
-    def _save_search_data(self, search_data, drop_duplicates):
-        search_data = self.sd_conv.func2str(search_data)
-
-        search_data_loaded = self._load()
-        if not self.replace_existing and search_data_loaded is not None:
-            search_data = pd.concat([search_data, search_data_loaded], axis=0)
-
-        if drop_duplicates:
-            search_data.drop_duplicates(subset=self.para_names, inplace=True)
-
-        with atomic_overwrite(self.search_data_path) as f:
-            search_data.to_csv(f, index=False)
 
     def _init_data_path(self, nth_process):
         # create file name for processes
@@ -68,7 +37,38 @@ class LongTermMemory(DataIO):
 
     def _append(self, para_dict, drop_duplicates):
         search_data = pd.DataFrame(para_dict, index=[0])
-        self._save_search_data(search_data, drop_duplicates)
+        self.save_search_data(search_data, drop_duplicates)
+
+
+class LongTermMemory(LTMBackend):
+    def __init__(self, path):
+        super().__init__(path)
+
+    def init_study(
+        self,
+        objective_function,
+        search_space,
+        study_id,
+        model_id,
+        replace_existing=False,
+        drop_duplicates=True,
+    ):
+        self.objective_function = objective_function
+        self.search_space = search_space
+
+        self.replace_existing = replace_existing
+        self.drop_duplicates = drop_duplicates
+
+        self.init_paths(study_id, model_id)
+
+        self.para_names = list(search_space.keys())
+
+        self.sd_conv = SearchDataConverter(search_space)
+
+        if not os.path.exists(self.model_path + "objective_function.pkl"):
+            self.save_objective_function(study_id, model_id)
+
+        self.clean_files(drop_duplicates)
 
     def clean_files(self, drop_duplicates):
         search_data_proc_paths = glob.glob(
@@ -95,43 +95,11 @@ class LongTermMemory(DataIO):
             if os.path.exists(search_data_proc_path):
                 os.remove(search_data_proc_path)
 
-    def init_experiment(
-        self,
-        objective_function,
-        search_space,
-        study_id,
-        model_id,
-        replace_existing=False,
-        drop_duplicates=True,
-    ):
-        self.objective_function = objective_function
-        self.search_space = search_space
-
-        self.replace_existing = replace_existing
-        self.drop_duplicates = drop_duplicates
-
-        self._init_paths(model_id, study_id)
-        self.para_names = list(search_space.keys())
-
-        self.sd_conv = SearchDataConverter(search_space)
-
-        # create directories if they do not exist
-        if not os.path.exists(self.model_path):
-            os.makedirs(self.model_path, exist_ok=True)
-        elif not os.path.exists(self.ltm_path):
-            os.makedirs(self.ltm_path, exist_ok=True)
-
-        if not os.path.exists(self.model_path + "objective_function.pkl"):
-            with open(self.model_path + "objective_function.pkl", "wb") as output_file:
-                dill.dump(self.objective_function, output_file)
-
-        self.clean_files(drop_duplicates)
-
     def load(self):
         return self._load()
 
     def save_on_finish(self, dataframe):
-        self._save_search_data(dataframe, self.drop_duplicates)
+        self.save_search_data(dataframe, self.drop_duplicates)
 
     def save_on_iteration(self, data_dict, nth_process=None):
         self._init_data_path(nth_process)
