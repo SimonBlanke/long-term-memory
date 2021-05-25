ce705fd5bcfa7b5b3b51cd7b9b6c5203a97a353e
Simon Blanke <simon.blanke@yahoo.com>
2021-05-25 14:12:25 +0200

    update wrapper to new tde version

--- hyperactive_long_term_memory/tde_wrapper.py
+++ hyperactive_long_term_memory/tde_wrapper.py
@@ -7,4 +7,4 @@ from tabular_data_explorer.streamlit_elements import create_streamlit_setup
 
 
 def open_tde(search_data):
-    create_streamlit_setup(search_data, default_plots=None)
+    create_streamlit_setup(search_data, plots=None)
