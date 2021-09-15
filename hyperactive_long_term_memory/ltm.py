2f4c8f38d082971612a589af5b50102920f23621
Simon Blanke <simon.blanke@yahoo.com>
2021-09-15 15:21:07 +0200

    change ltm api

--- hyperactive_long_term_memory/ltm.py
+++ hyperactive_long_term_memory/ltm.py
@@ -7,10 +7,12 @@ import glob
 import dill
 from numpy import isin
 import pandas as pd
+from hyperactive_data_storage import DataCollector
+
 
 from .search_data_converter import SearchDataConverter
 from .open_dashboard import open_dashboard
-from .data_io import DataIO, Paths
+from .data_io import Paths
 
 pd.options.mode.chained_assignment = None
 
@@ -18,8 +20,41 @@ pd.options.mode.chained_assignment = None
 class LongTermMemory(Paths):
     def __init__(self, path):
         super().__init__(path)
+        self.setup_ok_ = False
 
-    def init_study(
+    def load(self):
+        return self.data_c.load()
+
+    def save(self, search_data):
+        self.data_c.save(search_data)
+
+    def track(self, objective_function):
+        self.objective_function_id = objective_function.__name__
+
+        def wrapper(para):
+            results = objective_function(para)
+
+            if isinstance(results, tuple):
+                score = results[0]
+                results_dict = results[1]
+            else:
+                score = results
+                results_dict = {}
+
+            results_dict["score"] = score
+
+            ltm_data_dict = para.para_dict
+            ltm_data_dict.update(results_dict)
+
+            self.data_c.append(ltm_data_dict)
+
+            return results
+
+        wrapper.__name__ = objective_function.__name__
+
+        return wrapper
+
+    def setup(
         self,
         objective_function,
         search_space,
@@ -28,6 +63,8 @@ class LongTermMemory(Paths):
         replace_existing=False,
         drop_duplicates=True,
     ):
+        self.setup_ok_ = True
+
         self.objective_function = objective_function
         self.search_space = search_space
 
@@ -38,6 +75,7 @@ class LongTermMemory(Paths):
 
         self.para_names = list(search_space.keys())
 
+        self.data_c = DataCollector(self.search_data_path)
         self.sd_conv = SearchDataConverter(search_space)
 
         if not os.path.exists(self.model_path + "objective_function.pkl"):
